e664b7d295b6d8b4a11b69ea66fc841596d376a5
EddyRafidison <eddy.heriniaina.rafidison@gmail.com>
2025-07-07 10:07:25 +0000

    mod

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json({ limit: '50mb' }))
 app.use(express.urlencoded({ limit: '50mb', extended: true }))
-app.usa(httpsRedirect)
+app.use(httpsRedirect)
 
 // on monte toutes les routes définies dans routes.js
 app.use('/', routes)
